7b8b53c129fadd5b27521ed97dbc8863889a054e
George Czabania <george@mish.guru>
2018-12-15 11:50:10 +1300

    Retry if amqp server is down

--- packages/turbine-driver-amqplib/src/index.ts
+++ packages/turbine-driver-amqplib/src/index.ts
@@ -1,5 +1,6 @@
 import memoize from 'memoizee'
-import amqp from 'amqplib'
+import amqp, { Channel } from 'amqplib'
+import delay from 'delay'
 import  { Message, SubscribeOptions } from '@mishguru/turbine-types'
 
 import subscribe from './subscribe'
@@ -7,11 +8,18 @@ import publish from './publish'
 
 import { EXCHANGE } from './config'
 
-const createChannel = async (url: string) => {
-  const conn = await amqp.connect(url)
-  const channel = await conn.createChannel()
-  channel.assertExchange(EXCHANGE, 'topic', { durable: false })
-  return channel
+const createChannel = async (url: string): Promise<Channel> => {
+  try {
+    const conn = await amqp.connect(url)
+    const channel = await conn.createChannel()
+    channel.assertExchange(EXCHANGE, 'topic', { durable: false })
+    return channel
+  } catch (error) {
+    console.error(error)
+    console.log('Could not create channel. Waiting 10 seconds...')
+    await delay(10 * 1000)
+    return createChannel(url)
+  }
 }
 
 const assertChannel = memoize(createChannel, { promise: true })

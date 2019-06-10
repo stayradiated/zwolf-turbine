7b2f2dfb1d089b613b71e11083ff4c5ba7ad8dd7
Brendon John Muschamp <brendon@mish.guru>
2019-06-10 14:00:41 +1200

    Ch22913/topic for retrying subscription requests (#15)
    * Remove(fanservice-lite)
    
    * feature(clean command) so that we can fix the linked dependencies

--- packages/turbine-driver-fanservice-lite/README.md
+++
@@ -1,5 +0,0 @@
-# @mishguru/turbine-fanservice-driver-lite
-
-A lightweight turbine driver for fanservice that is used for only publishing.
-
-This driver can be used when we know that it's not necessary to subscribe to incoming topics
--- packages/turbine-driver-fanservice-lite/src/subscribe.ts
+++
@@ -1,9 +0,0 @@
-import { SubscribeOptions } from '@mishguru/turbine'
-
-type Fn = (a: SubscribeOptions) => Promise<void>
-
-const subscribe: Fn = async (options) => {
-  console.log('Ignoring subscription hook')
-}
-
-export { subscribe }

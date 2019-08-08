64a05ee8ff2890562b8a5125f44e88be6da2a9d3
George Czabania <1140167+stayradiated@users.noreply.github.com>
2019-08-08 12:06:42 +0200

    revert: use old message id if no turbine id exists

--- packages/turbine-utils-fanservice/src/parseFanserviceMessage.ts
+++ packages/turbine-utils-fanservice/src/parseFanserviceMessage.ts
@@ -1,4 +1,3 @@
-import { ulid } from 'ulid'
 import { decodeNameFromTopicArn } from '@mishguru/fanout-helpers'
 
 import { FANOUT_ENV } from './constants'
@@ -15,13 +14,14 @@ const parseFanserviceMessage = (message: FanserviceMessage) => {
       ? JSON.parse(message.Message)
       : {}
 
+  const messageId = message != null ? message.MessageId : undefined
   const timestamp =
     message != null ? new Date(message.Timestamp).getTime() : undefined
 
   const turbine = payload.__turbine__ != null ? payload.__turbine__ : {}
   delete payload.__turbine__
 
-  const id = turbine.id != null ? turbine.id : ulid()
+  const id = turbine.id != null ? turbine.id : messageId
   const parentId = turbine.parentId != null ? turbine.parentId : undefined
   const sentFrom = turbine.sentFrom != null ? turbine.sentFrom : undefined
   const sentAt = turbine.sentAt != null ? turbine.sentAt : timestamp
